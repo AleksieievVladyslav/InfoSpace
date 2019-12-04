54cbf557440e5337ae40eff86107216768289c82
AleksieievVladyslav <50144373+AleksieievVladyslav@users.noreply.github.com>
2019-12-04 16:59:22 +0200

    post query

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 express()
+.use(cors())
 .use(express.static(path.join(__dirname, 'public')))
 .engine('html', require('ejs').renderFile)
 .set('view engine', 'ejs')
 .get('/', (req, res) => {res.render('pages/index.ejs')})
+.post('/log/', (req, res) => {console.log('connection');res.send({status: 'ok'})})
 .listen(PORT, () => {console.log(`Listening on port ${PORT}...`)});
--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,26 @@
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
-$('')
+$(document).ready(function() {
+    $('#log').click(function(event) {
+        event.preventDefault()
+        $.ajax({
+            url: "https://info-space-admin.herokuapp.com/log/",
+            type: "POST",
+            data: {name: "admin"},
+            dataType: "html",
+            beforeSend: beforeSend,
+            success: success,
+            error: error
+        })
+    })
+})
+function beforeSend() {
+    console.log('sending...');
+}
+function success(data) {
+    console.log(data);
+}
+function error(e) {
+    console.log(e);
+}
