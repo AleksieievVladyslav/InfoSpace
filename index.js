8e240ceb2c05fa31675b6b9bf84887a7bb3d87f3
AleksieievVladyslav <50144373+AleksieievVladyslav@users.noreply.github.com>
2019-12-04 16:38:19 +0200

    just a litle bit update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 express()
 .use(express.static(path.join(__dirname, 'public')))
+.engine('html', require('ejs').renderFile)
 .set('view engine', 'ejs')
-.get('/', (req, res) => {res.render('pages/index.html')})
+.get('/', (req, res) => {res.render('pages/index.ejs')})
 .listen(PORT, () => {console.log(`Listening on port ${PORT}...`)});
